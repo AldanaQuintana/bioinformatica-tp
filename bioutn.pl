121c18d74f6d54cb2c4ae11dad93c68c71b2f60f
Aldana Quintana <aldanaqm@gmail.com>
2015-11-09 23:26:08 -0300

    Agrego punto 3 y output de una corrida

--- bioutn.pl
+++ bioutn.pl
@@ -18,35 +18,44 @@ my $remoto;
 sub help
 {
    my $msg = shift;
-   
+
    print "$msg\n\n"
       if $msg;
-   
+
    print <<STOP;
 $0 [...] <Archivo 1, Archivo 2, ...>
 Trabajo Práctico Final Bioinformática 2C 2015
 
    --punto, -p
       Selecciona el modo de ejecución, correpondiente al punto del TP:
-      
+
         1: Conversion de secuencias de nucleótidos GenBank a FASTA en todos los marcos
            de lectura posibles.
-           
+
            Ejemplo: $0 --punto 1 data/HBB_mRNA.gb
-           
+
         2: BLAST+ sobre secuencias FASTA, local o remoto. Para realizar ejecuciones
            locales es necesario contar con la base swissprot descomprimida en
            db/swissprot (puede ser obtenida en la dirección
            ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/swissprot.gz). La creación de la
            base de datos para el algoritmo se realiza la primera vez que es usada de
            forma interna.
-        
+
            Ejemplo de ejecución remota: $0 --punto 2 -r data/HBB_mRNA_aa.fas
            Ejemplo de ejecución local:  $0 --punto 2    data/HBB_mRNA_aa.fas
-           
+
+        3: Parsea un output de blast (por ejemplo, el resultado del ejercicio 2)
+           con un pattern como parámetro
+           y da como resultado un archivo con la lista de los hits
+           que encuentren una coincidencia con ese pattern.
+           El pattern debe ser una palabra sin espacios.
+
+           Ejemplo: $0 --punto 3 data/HBB_mRNA_aa_blastp.out Hemoglobin
+           Listará todos los los hits (accession + descripción) que tengan al menos una coincidencia con "Hemoglobin"
+
    --remoto, -r
       Usar algoritmos remotos cuando sea posible (ver punto 2).
-   
+
    --help, --ayuda, -?, -h
       Muestra este mensaje.
 STOP
@@ -67,34 +76,34 @@ sub punto1
    {
       # Cambiar la extension del archivo de salida
       (my $file_out = $file_in) =~ s/\.[^.]+$/_aa.fas/;
-      
+
       # Leer archivo
       my $seqio_in = Bio::SeqIO->new(
          -file    => $file_in,
          -format  => 'genbank',
       );
-      
+
       my $seqio_out = Bio::SeqIO->new(
          -file    => ">$file_out",
          -format  => 'fasta',
       );
-   
+
       my $c = 0;
-      
+
       # Por cada una de las secuencias contenidas, traducir los 6
       # frames y escribir a un nuevo archivo .fas
       while (my $seq = $seqio_in->next_seq)
       {
          $c++;
-         
+
          my @aa = Bio::SeqUtils->translate_6frames($seq);
-      
+
          foreach my $aa (@aa)
          {
             $seqio_out->write_seq($aa);
          }
       }
-      
+
       print "Archivo $file_in: $c secuencias convertidas a $file_out\n";
    }
 }
@@ -105,10 +114,10 @@ sub punto2
    {
       # Cambiar la extension del archivo de salida
       (my $file_out = $file_in) =~ s/\.[^.]+$/_blastp.out/;
-      
+
       my $fac;
-      
-      if ($remoto) {   
+
+      if ($remoto) {
          $fac = Bio::Tools::Run::StandAloneBlastPlus->new(
             -db_name  => 'swissprot',
             -remote   => 1,
@@ -120,10 +129,10 @@ sub punto2
             -create   => 1,
          );
       }
-      
+
       print "Ejecutando BLASTp ", ($remoto? 'remoto': 'local'),
             " para las secuencias de $file_in ...\n";
-      
+
       $fac->blastp(
          -query   => $file_in,
          -outfile => $file_out,
@@ -131,11 +140,40 @@ sub punto2
    }
 }
 
+use Bio::SearchIO;
+
+sub punto3
+{
+
+  my ($file_location, $pattern) = @_;
+  my $matcher = qr/$pattern/;
+
+  (my $file_output_location = $file_location) =~ s/\.[^.]+$/_matching_hits.txt/;
+  open(my $file_output, '>', $file_output_location);
+
+  my $file = new Bio::SearchIO(-file => $file_location, -format => 'blast');
+
+  while(my $result = $file->next_result){
+    while(my $hit = $result->next_hit){
+      my $hit_description = $hit->description;
+
+      if($hit_description =~ $matcher){
+        print $file_output $hit->name;
+        print $file_output "\n";
+        print $file_output $hit_description;
+        print $file_output "\n\n";
+      }
+    }
+  }
+
+  close $file_output;
+}
+
 # Entry point
 {
    help("Modo de operación (punto) no especificado.")
       unless $punto;
-   
+
    help("Ningún archivo especificado")
       unless @ARGV;
 
@@ -143,7 +181,9 @@ sub punto2
       punto1(@ARGV);
    } elsif ($punto == 2) {
       punto2(@ARGV);
-   } else {
+   } elsif ($punto == 3){
+      punto3(@ARGV)
+    } else {
       help("Punto $punto desconocido.");
    }
 }
